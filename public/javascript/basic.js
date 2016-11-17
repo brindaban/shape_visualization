320452ef0ddbfe9e6d534ec4cbf8ed94fda7d5f8
brindaban <brindaba@thoughtworks.com>
2016-11-17 13:11:34 +0530

    refactored and using single svg

--- public/javascript/basic.js
+++ public/javascript/basic.js
@@ -1,31 +1,39 @@
-var createSvg = function(elementName){
+var giveTrianglePointsValue = function(base,height){
+	return '0,'+height+' '+base+','+height+' '+(base/2)+',0';
+}
+var createTranslate = function(x,y){
+	return 'translate('+x+','+y+')';
+}
+var createProperty = function(elementType, attributes, className){
+	return {element:elementType, attr:attributes, className:className};
+}
+var createSvg = function(){
 	return d3.select('.shape')
 		.append('svg')
-		.attr('width',100)
-		.attr('height',100)
-		.style('padding','25px')
-		.append(elementName);
+		.attr('width',1000)
+		.attr('height',1000)
+		.style('padding','10px');
 }
-
-var drawShape = function(shapeProperty){
-	let element = createSvg(shapeProperty['element']);
+var drawShape = function(shapeProperty, position){
 	let attributes = shapeProperty['attr']
+	let element = d3.select('svg').append('g')
+		.attr('transform',createTranslate(150*position,0))
+		.classed(shapeProperty['className'],'true');
+
+	let shape = element.append(shapeProperty['element']);
 	for(eachAttr in attributes)
-		element.attr(eachAttr, attributes[eachAttr]);
-
-	let styles = shapeProperty['style'];
-	for(eachStyle in styles)
-		element.style(eachStyle,styles[eachStyle]);
+		shape.attr(eachAttr, attributes[eachAttr]);
 }
-
 var show = function(){
-	let	shapesWithProperties = [{element:'line', attr:{x1:0,y1:100,x2:100,y2:0}, style:{stroke:'grey'}},
-								{element:'circle', attr:{cx:50,cy:50,r:50}, style:{stroke:'red'}},
-								{element:'rect', attr:{width:100,height:100}, style:{stroke:'blue'}},
-								{element:'polygon',attr:{points: "0,100 100,100 50,0"},style:{stroke:'green'}}];
-
-	shapesWithProperties.forEach(function(eachShape){
-		drawShape(eachShape);
+	createSvg();
+	let shapesWithProperties = [
+			createProperty('line', {x1:0,y1:100,x2:100,y2:0}, 'line'),
+			createProperty('circle', {cx:50,cy:50,r:50}, 'circle'),
+			createProperty('rect',{width:100,height:100}, 'square'),
+			createProperty('polygon',{points:giveTrianglePointsValue(100,100)}, 'triangle')
+		];
+	shapesWithProperties.forEach(function(eachShape,index){
+		drawShape(eachShape,index);
 	});
 }
 window.onload = show;
